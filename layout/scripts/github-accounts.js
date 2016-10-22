06c5b2eeaca2288479e5258af9f32def1abd6bfd
seunkoko <seunowonikoko@gmail.com>
2016-10-22 17:39:09 +0100

    my portfolio page

--- layout/scripts/github-accounts.js
+++ layout/scripts/github-accounts.js
@@ -51,33 +51,33 @@ $('#perform_submit').click(function (){
         function display_feedback(value, string_value){
             if(value === 0){
                 if(string_value === "following"){
-                    $('#feed_back_following').html("<p class='lead text-center' style='color:#330000; font-size:13px'>Following: You no wan follow person?</p>");
+                    $('#feed_back_following').html("<p style='color:#FF0000;'>Following: You no wan follow person?</p>");
                 }else if(string_value === "followers"){
-                    $('#feed_back_followers').html("<p class='lead text-center' style='color:#330000; font-size:13px'>Followers: People no like you?</p>");
+                    $('#feed_back_followers').html("<p style='color:#FF0000;'>Followers: People no like you?</p>");
                 }else if(string_value === "repos"){
-                    $('#feed_back_repos').html("<p class='lead text-center' style='color:#330000; font-size:13px'>Repositories: You no wan create projects at all?</p>");
+                    $('#feed_back_repos').html("<p style='color:#FF0000;'>Repositories: You no wan create projects at all?</p>");
                 }else if(string_value === "gists"){
-                    $('#feed_back_gists').html("<p class='lead text-center' style='color:#330000; font-size:13px'>Gist: You no dey yan at all?</p>");
+                    $('#feed_back_gists').html("<p style='color:#FF0000;'>Gist: You no dey yan at all?</p>");
                 }           
             }else if(value <= 20){
                 if(string_value === "following"){
-                    $('#feed_back_following').html("<p class='lead text-center' style='color:#330099; font-size:14px'>Following: Try follow more people and f**k more projects</p>");
+                    $('#feed_back_following').html("<p style='color:#6600FF;'>Following: Try follow more people and f**k more projects</p>");
                 }else if(string_value === "followers"){
-                    $('#feed_back_followers').html("<p class='lead text-center' style='color:#330099; font-size:14px'>Followers: ummmm, you try small</p>");
+                    $('#feed_back_followers').html("<p style='color:#6600FF;'>Followers: ummmm, you try small</p>");
                 }else if(string_value === "repos"){
-                    $('#feed_back_repos').html("<p class='lead text-center' style='color:#330099; font-size:14px'>Repositories: You don try no be small</p>");
+                    $('#feed_back_repos').html("<p style='color:#6600FF;'>Repositories: You don try no be small</p>");
                 }else if(string_value === "gists"){
-                    $('#feed_back_gists').html("<p class='lead text-center' style='color:#330099; font-size:14px'>Gist: Confirm! i like your style, you no dey talk too much</p>");
+                    $('#feed_back_gists').html("<p style='color:#6600FF;'>Gist: Confirm! i like your style, you no dey talk too much</p>");
                 }    
             }else if(value > 20){
                 if(string_value === "following"){
-                    $('#feed_back_following').html("<p class='lead text-center' style='color:#000099; font-size:15px'>Following: You go dey f**k people code gannn</p>");
+                    $('#feed_back_following').html("<p style='color:#000099;'>Following: You go dey f**k people code gannn</p>");
                 }else if(string_value === "followers"){
-                    $('#feed_back_followers').html("<p class='lead text-center' style='color:#000099; font-size:15px'>Followers: Oh boy!, people like you wella</p>");
+                    $('#feed_back_followers').html("<p style='color:#000099;'>Followers: Oh boy!, people like you wella</p>");
                 }else if(string_value === "repos"){
-                    $('#feed_back_repos').html("<p class='lead text-center' style='color:#000099; font-size:15px'>Repositories: Correct Programmer!</p>");
+                    $('#feed_back_repos').html("<p style='color:#000099;'>Repositories: Correct Programmer!</p>");
                 }else if(string_value === "gists"){
-                    $('#feed_back_gists').html("<p class='lead text-center' style='color:#000099; font-size:15px'>Gist: You be broadcaster?</p>");
+                    $('#feed_back_gists').html("<p style='color:#000099;'>Gist: You be broadcaster?</p>");
                 }  
             }
         }
@@ -90,7 +90,7 @@ $('#perform_submit').click(function (){
 
 
         //manipulating the outputed values in html
-        $('#avatar_url').html("<p class='lead' style='font-size:15px'>" + account_name +"'s profile picture: &nbsp</p><img src=" + avatar_url_use + " style = 'height:45px;width:70px'></img>");
+        $('#avatar_url').html("<p>" + account_name +"'s profile picture: &nbsp</p><img src=" + avatar_url_use + " style = 'height:50px;width:70px;border-radius:5%'></img>");
         $('#public_repos').html("Public Repositories: " + public_repos_use);
         $('#public_gists').html("public gists: " + public_gists_use);
         $('#followers').html("Followers: " + followers_use);
@@ -98,8 +98,9 @@ $('#perform_submit').click(function (){
     })
           .fail(function(data) {
             //alert( "error" );
+            //"<p class='lead text-center' style='color:#800000; margin-top:10px; margin-bottom:10px'>User does not exist<p>"
                 console.log("not found");
-                $('#public_gists').html("<p class='lead text-center' style='color:#800000; margin-top:10px; margin-bottom:10px'>User does not exist<p>");
+                $('#public_gists').html("");
                 $('#avatar_url').empty("");
                 $('#public_repos').html("");
                 $('#followers').html("");
@@ -108,6 +109,7 @@ $('#perform_submit').click(function (){
                 $('#feed_back_following').html("");
                 $('#feed_back_repos').html("");
                 $('#feed_back_gists').html("");
+                alert("User does not exist");
           })
             .always(function() {
               //alert( "finished" );
